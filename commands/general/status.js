2552a19c2a4d0a23f1cba59b9351866a5ca38ff9
mosesbr88 <mosesbr88@hotmail.com>
2026-04-09 15:38:16 +0530

    Update status.js

--- commands/general/status.js
+++ commands/general/status.js
@@ -1,15 +1,30 @@
+const os = require("os");
+
 module.exports = {
   name: "status",
   execute(ctx) {
+    const pingStart = Date.now();
+    const msg = await ctx.reply("⏳ Checking status...");
+    const ping = Date.now() - pingStart;
+
+    const cpuLoad = os.loadavg()[0].toFixed(2);
+
+  // RAM
+    const totalRAM = (os.totalmem() / 1024 / 1024).toFixed(0);
+    const freeRAM = (os.freemem() / 1024 / 1024).toFixed(0);
+    const usedRAM = totalRAM - freeRAM;
+    
+    
     let uptime = formatTime(Date.now() - serverStartTime);
-      const text = `
+      
+    const text = `
 ╭━━━〔 🚀 BOT STATUS 〕━━━╮
 
 ⏱️ *Uptime:* \`${uptime}\`
-📶 *Ping:* \`-1 ms\`
+📶 *Ping:* \`${ping} ms\`
 
-🖥️ *CPU Load:* \`{cpuLoad}\`
-💾 *RAM:* \`{usedRAM}MB / {totalRAM}MB\`
+🖥️ *CPU Load:* \`${cpuLoad} %\`
+💾 *RAM:* \`${usedRAM}MB / ${totalRAM}MB\`
 📦 *Storage:* \`{st.used} / ${1024 * 4} GB\`
 
 🗄️ *Database:* Connected 🟢
