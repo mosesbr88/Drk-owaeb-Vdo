42c9a065c5e83d23180135877cf6b8a827ac9103
mosesbr88 <mosesbr88@hotmail.com>
2026-04-09 13:10:50 +0530

    Update start.js

--- commands/general/start.js
+++ commands/general/start.js
@@ -10,6 +10,7 @@ module.exports = {
       ctx.reply(`<pre>${JSON.stringify(deta, null, 2)}</pre>`, {
   parse_mode: "HTML"
 });
+      return;
     }
     if(!args || args.length < 1){
       ctx.reply("👋 Welcome!");
@@ -26,9 +27,9 @@ module.exports = {
         return ctx.reply(`❌| Invalid or expired Referral Code Provided..`);
       }else if(!RFCode[args[0]].ttl || isNaN(RFCode[args[0]].ttl) || RFCode[args[0]].ttl < Date.now()){
         return ctx.reply("⌛| Your Referal Code is expired. please create new one");
-      }else if(RFCode[args[0]].createdBy === ctx.from.id){
+      } /*else if(RFCode[args[0]].createdBy === ctx.from.id){
         return ctx.reply("👾 | You cannot use your own Referral Code. Sent it to your friends!");
-      }
+      }*/
       const RFUser = await bot.api.getChat(RFCode[args[0]].createdBy);
       if(!RFUser){ return ctx.reply("⚠️ | Referal creator is not a valid user"); }
       await db.set(`users.${ctx.from.id}`, {
