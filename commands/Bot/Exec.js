cfb56922e4acf0d1905517b02f3c3af26419b120
mosesbr88 <mosesbr88@hotmail.com>
2026-04-09 11:33:39 +0530

    Update Exec.js

--- commands/Bot/Exec.js
+++ commands/Bot/Exec.js
@@ -1,9 +1,11 @@
 module.exports = {
   name: "eval",
   aliases: ["run"],
-
+  isOwner: true,
   execute(ctx) {
-     }
+    execState.set(ctx.from.id, true);
+    return ctx.reply("🟢 Eval mode ON\nSend code...\nType /exit to stop");
+  }
 };
 
 
