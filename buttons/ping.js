9a1d00e07b81591c06b40388124520b264eccdaf
mosesbr88 <mosesbr88@hotmail.com>
2026-04-09 14:47:31 +0530

    Update ping.js

--- buttons/ping.js
+++ buttons/ping.js
@@ -2,7 +2,11 @@ module.exports = {
   name: "ping_btn",
 
   async execute(ctx) {
-    await ctx.answerCallbackQuery();
-    ctx.reply("🏓 Button Pong");
+    //await ctx.answerCallbackQuery();
+   await ctx.answerCallbackQuery({
+  text: "YOU CLICKED THE BUTTON",
+  show_alert: true
+});
+    //ctx.reply("🏓 Button Pong");
   }
 };
