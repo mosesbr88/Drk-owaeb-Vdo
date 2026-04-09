f5deb0500d1ce9203686582f88de03ab05f2ed21
mosesbr88 <mosesbr88@hotmail.com>
2026-04-09 16:00:05 +0530

    Update eventHandler.js

--- handlers/eventHandler.js
+++ handlers/eventHandler.js
@@ -4,9 +4,11 @@ const path = require("path");
 module.exports = (bot) => {
   const files = fs.readdirSync(path.join(__dirname, "../events"));
 
+  bot.EventListenerCount = 0;
+  
   for (const file of files) {
     const event = require(`../events/${file}`);
-
+bot.EventListenerCount += 1;
     bot.on(event.event, (ctx, next) => {
       if (!ctx.from || ctx.from.is_bot) return next();
       event.execute(ctx, bot);
