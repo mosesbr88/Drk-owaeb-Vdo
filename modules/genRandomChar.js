f5ae28174dbf6b0645b3afc1109ab333e244ec6a
mosesbr88 <mosesbr88@hotmail.com>
2026-04-11 14:20:47 +0530

    Update and rename genRandomChar.js to generateRandomToken.js

--- modules/genRandomChar.js
+++
@@ -1,3 +0,0 @@
-const crypto = require("crypto"),
-CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-function generateReferralToken(length = 10) { let result = ""; while (result.length < length) { const byte = crypto.randomBytes(1)[0]; if (byte < 62 * 4) { result += CHARS[byte % 62]; }}; return result;}
---
+++ modules/generateRandomToken.js
@@ -0,0 +1,4 @@
+const crypto = require("crypto"),
+CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+function generateRandomToken(length = 10) { let result = ""; while (result.length < length) { const byte = crypto.randomBytes(1)[0]; if (byte < 62 * 4) { result += CHARS[byte % 62]; }}; return result;}
+module.exports = generateRandomToken;
