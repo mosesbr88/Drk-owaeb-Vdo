c5915f02e54e85ebd20d134fd6595aa06ef04447
mosesbr88 <mosesbr88@hotmail.com>
2026-04-11 18:24:49 +0530

    Update tgLogger.js

--- utils/tgLogger.js
+++ utils/tgLogger.js
@@ -1,4 +1,4 @@
-module.exports = (bot) => {
+/*module.exports = (bot) => {
   const loggingGroup = -1003935136005;
 
   return {
@@ -13,6 +13,39 @@ module.exports = (bot) => {
 
         // send to telegram group
         await bot.api.sendMessage(loggingGroup, `📌 ${message}`);
+      } catch (err) {
+        console.error("TG Logger Error:", err);
+      }
+    },
+  };
+};
+*/
+
+module.exports = (bot) => {
+  const loggingGroup = -1003935136005;
+
+  return {
+    log: async (...args) => {
+      try {
+        let text = "";
+        let extra = {};
+
+        // if last arg is object → treat as options
+        if (typeof args[args.length - 1] === "object") {
+          extra = args.pop();
+        }
+
+        // build message from remaining args
+        text = args
+          .map((a) =>
+            typeof a === "object"
+              ? JSON.stringify(a, null, 2)
+              : String(a)
+          )
+          .join(" ");
+
+        await bot.api.sendMessage(loggingGroup, text, extra);
+
       } catch (err) {
         console.error("TG Logger Error:", err);
       }
