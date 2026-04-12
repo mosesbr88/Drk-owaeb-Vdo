0e8299dafd6ad038559cd6159f650bec67539261
mosesbr88 <mosesbr88@hotmail.com>
2026-04-12 20:41:43 +0530

    Create videos.js

---
+++ commands/vid/videos.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: "videos",
+  description: "Get Videos from the bot",
+  execute(bot, ctx) {
+    return ctx.reply("🟢 Eval mode ON\nSend code...\nType /exit to stop");
+  }
+};
