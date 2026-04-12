52b844395bcd009b7d7fec0f7a286920f727a8ba
mosesbr88 <mosesbr88@hotmail.com>
2026-04-12 21:21:20 +0530

    Update and rename videos.js to vdos.js

--- commands/vdo/vdos.js
+++ commands/vdo/vdos.js
@@ -1,17 +1,23 @@
 const { Keyboard } = require("grammy");
+let vdoConfig = require("../../vdoConfig");
 
 module.exports = {
   name: "videos",
   description: "Get Videos from the bot",
   execute(bot, ctx) {
 
-    const keyboard = new Keyboard()
-      .text("🎬 Video 1")
+    const keyboard = new Keyboard().resized();
+
+      for(let i=0; i<vdoConfig.length; i++){
+        keyboard.text(vdoConfig[i].);
+      };
+          
+    /*  .text("🎬 Video 1")
       .text("🎬 Video 2").row()
       .text("🎬 Video 3")
       .text("🎬 Video 4").row()
       .text("🎬 Video 5")
-      .resized(); // mobile friendly
+      .resized(); // mobile friendly*/
 
     return ctx.reply(
       "✅",
