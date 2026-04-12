e6243ee5f9ef01db9b88e6d38d27c3a791a4325a
mosesbr88 <mosesbr88@hotmail.com>
2026-04-12 20:50:03 +0530

    Update videos.js

--- commands/vid/videos.js
+++ commands/vid/videos.js
@@ -1,19 +1,23 @@
-const { InlineKeyboard } = require("grammy");
+const { Keyboard } = require("grammy");
 
 module.exports = {
   name: "videos",
   description: "Get Videos from the bot",
   execute(bot, ctx) {
 
-    const keyboard = new InlineKeyboard()
-      .text("🎬 Video 1", "video_1")
-      .text("🎬 Video 2", "video_2").row()
-      .text("🎬 Video 3", "video_3")
-      .text("🎬 Video 4", "video_4").row()
-      .text("🎬 Video 5", "video_5");
+    const keyboard = new Keyboard()
+      .text("🎬 Video 1")
+      .text("🎬 Video 2").row()
+      .text("🎬 Video 3")
+      .text("🎬 Video 4").row()
+      .text("🎬 Video 5")
+      .resized(); // mobile friendly
 
-    return ctx.reply("✅", {
-      reply_markup: keyboard
-    });
+    return ctx.reply(
+      "✅",
+      {
+        reply_markup: keyboard
+      }
+    );
   }
 };
