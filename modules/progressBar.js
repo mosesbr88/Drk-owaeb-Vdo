4a975c703833f1f6efbbfdb846566e2c22726583
mosesbr88 <mosesbr88@hotmail.com>
2026-04-12 20:38:05 +0530

    Create progressBar.js

---
+++ modules/progressBar.js
@@ -0,0 +1,12 @@
+function getProgress(current = 0, required = 20) {
+  const totalBars = 10;
+  const filledBars = Math.round((current / required) * totalBars);
+  const emptyBars = totalBars - filledBars;
+
+  const bar = "█".repeat(filledBars) + "░".repeat(emptyBars);
+  const percent = Math.min(100, Math.floor((current / required) * 100));
+
+  return { bar, percent };
+}
+
+modules.exports = getProgress;
