21ac501005c48c3efb366e714788e30fdc87ec35
mosesbr88 <mosesbr88@hotmail.com>
2026-04-12 21:33:58 +0530

    Update vdoConfig.js

--- vdoConfig.js
+++ vdoConfig.js
@@ -1,6 +1,7 @@
 module.exports = [
   { name: "🌽 Corn", id: "avengers" },
-  { name: "🎥 C¡C¡ TV", id: "silent" },
+  { name: "🎥 C¡C¡ TV", id: "silent", row: true },
+  { name: "💅 RP", id: "bio" },
   { name: "💀 Dw V¡d", id: "ironman", ref_req: 20 },
-  { name: "💦 C¡P¡", id: "batman", ref_req: 20 },
+  { name: "💦 C¡P¡", id: "batman", ref_req: 20, row: true },
 ];
