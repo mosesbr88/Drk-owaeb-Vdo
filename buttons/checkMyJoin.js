190e0b627f606509e5a6d9810a1a5ec20c687a7b
mosesbr88 <mosesbr88@hotmail.com>
2026-04-14 10:26:24 +0530

    Create checkMyJoin.js

---
+++ buttons/checkMyJoin.js
@@ -0,0 +1,24 @@
+module.exports = {
+  name: "check_my_join",
+
+  async execute(bot, ctx) {
+ const ok = await isUserJoined(ctx, ctx.from.id, true); // 👈 force
+
+  if (!ok) {
+    return ctx.answerCallbackQuery({
+      text: "❌ You haven't joined all channels yet!",
+      show_alert: true
+    });
+  }
+
+  try {
+    await ctx.deleteMessage();
+  } catch {}
+
+  await ctx.answerCallbackQuery({ text: "✅ Verified!" });
+
+  await ctx.reply(
+    `<b>🎉 Success!</b>\n\nNow you can use the bot.`,
+    { parse_mode: "HTML" }
+  );
+  }};
