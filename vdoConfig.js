dc271bade88ad278595c8af3b212be4707552ebd
mosesbr88 <mosesbr88@hotmail.com>
2026-04-15 00:28:40 +0530

    Update vdoConfig.js

--- vdoConfig.js
+++ vdoConfig.js
@@ -1,12 +1,12 @@
 module.exports = [
-  { name: `🌽 ${atob("QzBybg==")}`, id: "avengers", price: 2 },
-  { name: `🎥 ${atob("Qw==")}¡${atob("Qw==")}¡ ${atob("VFY=")}`, id: "silent", price: 6 },
-  { name: `🤞 ${atob("QQ==")}|${atob("MG4=")} ${atob("R3I=")}|`, id: "dot", row: true, price: 4 },
+  { name: `🌽 ${atob("QzBybg==")}`, id: "c_r_n", price: 2 },
+  { name: `🎥 ${atob("Qw==")}¡${atob("Qw==")}¡ ${atob("VFY=")}`, id: "sisiTevi", price: 6 },
+  { name: `🤞 ${atob("QQ==")}|${atob("MG4=")} ${atob("R3I=")}|`, id: "al0ngr", row: true, price: 4 },
   
-  { name: `🚷 ${atob("Q2dodCBSZCBIbmRlZA==")}`, ref_req: 1, id: "end", price: 20 },
-  { name: `🐙 ${atob("QW0=")}¡${atob("bg==")}|`, id: "dogu", ref_req: 2, row: true, price: 20 },
+  { name: `🚷 ${atob("Q2dodCBSZCBIbmRlZA==")}`, ref_req: 1, id: "cht_rh", price: 20 },
+  { name: `🐙 ${atob("QW0=")}¡${atob("bg==")}|`, id: "anmul", ref_req: 2, row: true, price: 20 },
   
-  { name: `💅 ${atob("UlA=")}`, id: "bio", ref_req: 5, price: 40 },
-  { name: `💀 ${atob("RHcgVg==")}¡${atob("ZA==")}`, id: "ironman", ref_req: 10, price: 30 },
-  { name: `💦 ${atob("Qw==")}¡${atob("UA==")}¡`, id: "batman", ref_req: 10, row: true, price: 50 },
+  { name: `💅 ${atob("UlA=")}`, id: "rdotp", ref_req: 5, price: 40 },
+  { name: `💀 ${atob("RHcgVg==")}¡${atob("ZA==")}`, id: "dew", ref_req: 10, price: 30 },
+  { name: `💦 ${atob("Qw==")}¡${atob("UA==")}¡`, id: "si_pi", ref_req: 10, row: true, price: 50 },
 ];
