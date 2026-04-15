edb11a3c9d16a721199204e221e6e5f3416bb99e
mosesbr88 <mosesbr88@hotmail.com>
2026-04-15 14:36:58 +0530

    Update slashHandler.js

--- handlers/slashHandler.js
+++ handlers/slashHandler.js
@@ -2,10 +2,12 @@ module.exports = async (bot) => {
   const commands = [];
 
   bot.commands.forEach((cmd) => {
+    if(!(cmd.hidden)){
     commands.push({
       command: cmd.name,
       description: cmd.description || "No desc"
     });
+    }
   });
 
   await bot.api.setMyCommands(commands);
